b7de5b69c11d9c520a38fe03fcaffdcded11bd66
Josh Worley <helloworley@gmail.com>
2023-08-12 21:48:19 -0400

    set up notion api call

--- next.config.js
+++ next.config.js
@@ -1,6 +1,38 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  experimental: {
+    appDir: true,
+    workerThreads: false,
+    cpus: 1,
+  },
+  images: {
+    domains: [],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            dimensions: false,
+          },
+        },
+      ],
+      type: "javascript/auto",
+      issuer: {
+        and: [/\.(js|ts)x?$/],
+      },
+    });
 
-module.exports = nextConfig
+    return config;
+  },
+
+  staticPageGenerationTimeout: 1000,
+
+  // security concerns
+  poweredByHeader: false,
+};
+
+module.exports = nextConfig;
