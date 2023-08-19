3e5e76f64857f739b05c37a17623cc3e42155ac0
Josh Worley <helloworley@gmail.com>
2023-08-19 17:22:04 -0400

    background image in BlurredBackground component

--- components/layout/BlurredBackground.tsx
+++ components/layout/BlurredBackground.tsx
@@ -1,5 +1,4 @@
-import Image from "next/image";
-import { NotionPropertyImage, NotionPropertyImageTypes } from "../common/NotionPropertyImage";
+import { NotionPropertyImageTypes } from "@/components/common/NotionPropertyImage";
 
 export default function BlurredBackground({
   children,
@@ -12,29 +11,31 @@ export default function BlurredBackground({
   bg?: string;
   className?: string;
 }) {
-  const renderBackgroundImage = () => {
-    if (typeof image === "string") {
-      return <Image src={image} alt="Background" layout="fill" objectFit="cover" objectPosition="center" className="fixed left-0 top-0 z-0" />;
-    } else if (image?.url) {
-      return (
-        <NotionPropertyImage
-          image={image}
-          alt="Background"
-          objectFit="cover"
-          className="left-50% top-50% -translate-x-50% -translate-y-50% absolute z-0 transform overflow-hidden"
-          width={3000}
-          height={3000}
-        />
-      );
-    }
-    return null;
-  };
+  // old code for displaying an image as an html element
+  // const renderBackgroundImage = () => {
+  //   if (typeof image === "string") {
+  //     return <Image src={image} alt="Background" layout="fill" objectFit="cover" objectPosition="center" className="fixed left-0 top-0 z-0" />;
+  //   } else if (image?.url) {
+  //     return (
+  //       <NotionPropertyImage
+  //         image={image}
+  //         alt="Background"
+  //         objectFit="cover"
+  //         className="left-50% top-50% -translate-x-50% -translate-y-50% absolute z-0 transform overflow-hidden"
+  //         width={3000}
+  //         height={3000}
+  //       />
+  //     );
+  //   }
+  //   return null;
+  // };
+
+  const _url = typeof image === "string" ? image : image.url;
 
   return (
     <div className={["relative flex h-screen flex-col", className].join(" ")}>
       {/* Background Image */}
-      {/* {image && renderBackgroundImage()} */}
-      <div className="fixed bottom-0 left-0 right-0 top-0 bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${image})` }}></div>
+      <div className="fixed bottom-0 left-0 right-0 top-0 bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${_url})` }}></div>
 
       {/* Blurred Overlay */}
       <div className={`${bg} fixed bottom-0 left-0 right-0 top-0 z-0 backdrop-blur-3xl`}></div>
