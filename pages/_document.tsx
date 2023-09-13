7a123056673281fb132b0c9957f31dfe1c4de80b
Josh Worley <helloworley@gmail.com>
2023-09-13 17:15:19 -0400

    new typekit link

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>{/* <link rel="stylesheet" href="https://use.typekit.net/ilm5wgd.css" /> */}</Head>
+      <Head>
+        <link rel="stylesheet" href="https://use.typekit.net/zbc6lde.css" />
+      </Head>
       <body>
         <Main />
         <NextScript />
