a8b19e3fd14524adaa0d65be683fd6036c8bf0ff
Josh Worley <helloworley@gmail.com>
2023-09-17 17:39:29 -0400

    about.tsx prerender fix

--- pages/about.tsx
+++ pages/about.tsx
@@ -56,7 +56,8 @@ export default function Home({ data }) {
 
 export const getStaticProps = async () => {
   try {
-    const response = await fetch(process.env.NEXT_NOTION_API_URL);
+    // const response = await fetch(process.env.NEXT_NOTION_API_URL);
+    const response = await fetch(`${process.env.NEXT_NOTION_API_URL}/notion`);
     if (!response.ok) {
       throw new Error(`Network response was not ok - ${response.statusText}`);
     }
