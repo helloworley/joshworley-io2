9881c8aa3b58a90cd1914b5a8ea5119647e5754f
Josh Worley <helloworley@gmail.com>
2023-09-17 12:38:08 -0400

    Analytics in the _app.tsx file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,17 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import RootLayout from "@/components/layout/layout";
 import { SkrimProvider } from "@/components/skrim/SkrimProvider";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SkrimProvider>
-      <RootLayout>
-        <Component {...pageProps} />
-      </RootLayout>
-    </SkrimProvider>
+    <>
+      <SkrimProvider>
+        <RootLayout>
+          <Component {...pageProps} />
+        </RootLayout>
+      </SkrimProvider>
+      <Analytics />
+    </>
   );
 }
