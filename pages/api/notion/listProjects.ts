d34ca11d9611d1352b5b44ee686700163c1e81e1
Josh Worley <helloworley@gmail.com>
2023-09-17 17:44:57 -0400

    add project slugs to cache

--- pages/api/notion/listProjects.ts
+++ pages/api/notion/listProjects.ts
@@ -1,10 +1,18 @@
 import { getDatabase } from "./notion";
+import cache from "memory-cache";
+
 export const database = process.env.NOTION_PROJECTS_DATABASE;
 
 export default async (req, res) => {
   try {
-    const result = await getDatabase(database);
-    const slugs = result.map(page => ({ slug: page.properties.Slug?.rich_text[0]?.plain_text ?? "" }));
+    let slugs = cache.get("project-slugs");
+
+    if (!slugs) {
+      const result = await getDatabase(database);
+      slugs = result.map(page => ({ slug: page.properties.Slug?.rich_text[0]?.plain_text ?? "" }));
+      cache.put("project-slugs", slugs, 1800000); // Cache for 30 minutes
+    }
+
     res.status(200).json(slugs);
   } catch (error) {
     res.status(500).json({ error: "Internal Server Error" });
