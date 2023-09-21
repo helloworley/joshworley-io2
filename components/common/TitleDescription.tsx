3aab1075c3a1e3366127d7d6b799cf0484fb21f7
Josh Worley <helloworley@gmail.com>
2023-09-20 20:03:02 -0400

    projects

--- components/common/TitleDescription.tsx
+++ components/common/TitleDescription.tsx
@@ -1,6 +1,6 @@
 export default function TitleDescription({ title, description }) {
   return (
-    <div>
+    <div className="max-w-2xl">
       <h1 className="mb-4 text-4xl text-white">{title}</h1>
       <p className="text-white">{description}</p>
     </div>
