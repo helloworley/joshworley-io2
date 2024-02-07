2d5ae5f12a273870c36a2901939339794572d186
Josh Worley <helloworley@gmail.com>
2024-02-06 22:57:28 -0600

    technology to have img tag handling svg

--- components/common/Technology.tsx
+++ components/common/Technology.tsx
@@ -1,12 +1,10 @@
-import StrapiImage from "@/components/common/StrapiImage";
-
 export default function Technology({ tech }) {
   const { name, icon, type, url } = tech;
 
   return (
     <div className="flex grid-cols-[64px_1fr] items-center gap-2 md:gap-3">
       <a href={url} target="_blank" rel="noreferrer" className="block h-16 w-16">
-        <StrapiImage {...icon.data} />
+        <img src={icon?.data?.attributes?.url} alt={icon?.data?.attributes?.alternativeText} />
       </a>
       <div className="flex flex-col gap-0">
         <p className="text-white">{name}</p>
